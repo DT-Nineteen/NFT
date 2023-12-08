812ec8d58d33b1bda48b2ba5fb9556d6b14d711d
Trinh Duc Truong <truongtd@bsscomerce.com>
2023-12-09 02:21:59 +0700

    check tmp

--- src/pages/api/nft-storage.ts
+++ src/pages/api/nft-storage.ts
@@ -12,8 +12,8 @@ const handler: NextApiHandler = async (req, res) => {
   try {
     // Parse req body and save image in /tmp
     const data: any = await new Promise((res, rej) => {
-      const uploadDir = `/tmp`;
-      const form = formidable({ multiples: true, uploadDir });
+      const uploadDir = `tmp`;
+      const form = formidable({ multiples: true, 'tmp' });
       form.parse(req, (err, fields, files) => {
         if (err) rej(err);
         res({ ...fields, ...files });
